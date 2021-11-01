69f84534a79090d3ef2d900c6159d564f37f055b
maning <ningma1997@gmail.com>
2021-11-01 09:40:08 +0800

    Initial Commit

---
+++ StickerMator/StickerMatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerMatorApp.swift
+//  StickerMator
+//
+//  Created by Ning Ma on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct StickerMatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
